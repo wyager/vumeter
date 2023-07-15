78703036a7d9108834923285af368349e201aa83
Andrei Holingher <holyandrei@gmail.com>
2023-07-15 08:53:02 +0300

    remove debugging code

--- code/audio-board-2/program.sh
+++ code/audio-board-2/program.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-cargo objcopy --release -- -O ihex target/thumbv7em-none-eabihf/release/audio_board_h
-arm-none-eabi-objcopy -O ihex -R .eeprom ./target/thumbv7em-none-eabihf/release/audio_board_h ./audio_board_h.hex
-
-./teensy_loader_cli.exe --mcu=TEENSY40 -w -v ./audio_board_h.hex
+cargo objcopy --release -- -O ihex audio_board_h.hex
+#or do this
+#arm-none-eabi-objcopy -O ihex -R .eeprom ./target/thumbv7em-none-eabihf/release/audio_board_h ./audio_board_h.hex
+#you need to delete the first section from hex file
+teensy_loader_cli.exe --mcu=TEENSY40 -w -v audio_board_h.hex
--- code/audio-board-2/src/main.rs
+++ code/audio-board-2/src/main.rs
@@ -15,7 +15,6 @@ use teensy4_panic as _;
 use cortex_m::asm;
 use teensy4_bsp as bsp;
 use bsp::board;
-use bsp::hal::timer::Blocking;
 
 use vu::bandpass as bp;
 use vu::audio_hw::*;
@@ -25,15 +24,6 @@ use cortex_m::prelude::{_embedded_hal_serial_Read, _embedded_hal_serial_Write};
 
 use boardlib::{spdif,uart,switch,timer};
 use cortex_m::asm::wfi;
-
-
-use core::fmt::Write as _;
-
-/// CHANGE ME to vary the baud rate.
-const UART_BAUD: u32 = 115200;
-/// Milliseconds to delay before toggling the LED
-/// and writing text outputs.
-const DELAY_MS: u32 = 500;
 
 #[bsp::rt::entry]
 fn main() -> ! {
@@ -54,41 +44,14 @@ fn main() -> ! {
         // `pins` has objects that represent the physical pins. The object
         // for pin 13 is `p13`.
         pins,
-        // This is a hardware timer. We'll use it for blocking delays.
-        mut gpt1,
-        // These are low-level USB resources. We'll pass these to a function
-        // that sets up USB logging.
-        usb,
         // This is the GPIO2 port. We need this to configure the LED as a
         // GPIO output.
         mut gpio2,
-        // This resource is for the UART we're creating.
-        lpuart2,
         ..
     } = board::t40(instances);
 
-    // When this returns, you can use the `log` crate to write text
-    // over USB. Use either `screen` (macOS, Linux) or PuTTY (Windows)
-    // to visualize the messages from this example.
-    bsp::LoggingFrontend::default_log().register_usb(usb);
-
     // This configures the LED as a GPIO output.
     let led = board::led(&mut gpio2, pins.p13);
-
-    // Configures the GPT1 timer to run at GPT1_FREQUENCY. See the
-    // constants below for more information.
-    gpt1.disable();
-    gpt1.set_divider(GPT1_DIVIDER);
-    gpt1.set_clock_source(GPT1_CLOCK_SOURCE);
-
-    // Convenience for blocking delays.
-    let mut delay = Blocking::<_, GPT1_FREQUENCY>::from_gpt(gpt1);
-
-    // Create the UART driver using pins 14 and 15.
-    // Cast it to a embedded_hal trait object so we can
-    // use it with the write! macro.
-    let mut lpuart2: board::Lpuart2 = board::lpuart(lpuart2, pins.p14, pins.p15, UART_BAUD);
-    let lpuart2: &mut dyn embedded_hal::serial::Write<u8, Error = _> = &mut lpuart2;
 
     let mut counter: u32 = 0;
     
@@ -113,17 +76,6 @@ fn main() -> ! {
     
     loop {
         led.toggle();
-        log::info!("Hello from the USB logger! The count is {counter}");
-        write!(
-            lpuart2,
-            "Hello from the UART driver! The count is {counter}\r\n"
-        )
-        .ok();
-
-        delay.block_ms(DELAY_MS);
-        counter = counter.wrapping_add(1);
-	
-	
         // Flush major serial buffer to minor buffer until we run out of space in minor buffer
         while let Some(()) = tx_buf.with_front(|byte| uart.write(byte).ok()) {}
         // Check for hardware events
@@ -147,16 +99,3 @@ fn main() -> ! {
     }
 }
 
-// We're responsible for configuring our timers.
-// This example uses PERCLK_CLK as the GPT1 clock source,
-// and it configures a 1 KHz GPT1 frequency by computing a
-// GPT1 divider.
-use bsp::hal::gpt::ClockSource;
-
-/// The intended GPT1 frequency (Hz).
-const GPT1_FREQUENCY: u32 = 1_000;
-/// Given this clock source...
-const GPT1_CLOCK_SOURCE: ClockSource = ClockSource::HighFrequencyReferenceClock;
-/// ... the root clock is PERCLK_CLK. To configure a GPT1 frequency,
-/// we need a divider of...
-const GPT1_DIVIDER: u32 = board::PERCLK_FREQUENCY / GPT1_FREQUENCY;
